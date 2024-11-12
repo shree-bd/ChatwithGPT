dd782512a240553b6a1b0cacb1e248015817e889
shridharbhardwaj <shridharbhardwaj@gmail.com>
2024-11-12 15:52:43 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import openai
+
+openai.api_key = "Your API Key"
+
+
+def chat_with_gpt(chat_log):
+    response = openai.ChatCompletion.create(model='gpt-3.5-turbo',
+                                            messages=chat_log
+                                          )
+    return response.choices[0].message.content.strip()
+
+
+chat_log = []
+# Remembering more posts is more expensive
+n_remembered_post = 2
+
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ['quit', "exit", "bye"]:
+            break
+
+        chat_log.append({'role': 'user', 'content': user_input})
+
+        if len(chat_log) > n_remembered_post:
+            del chat_log[:len(chat_log)-n_remembered_post]
+
+        response = chat_with_gpt(chat_log)
+        print("Chatbot:", response)
+        chat_log.append({'role': "assistant", 'content': response})
